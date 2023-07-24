0f591a45e352efc98acc9f19b448bf3cdc8edddd
Shriram <shriramrayakar@gmail.com>
2023-07-24 09:48:58 +0530

    sixth

--- server/index.js
+++ server/index.js
@@ -1,11 +1,36 @@
 const express = require("express");
 var cron = require('node-cron');
 var mysql = require('mysql');
+const https = require('https');
 var result = []
 
-  cron.schedule('0 */10 * * * *', function() {
-    console.log('rappa pa pa pa ')
-    return 1+1
+  cron.schedule('* * * * * *', function() {
+    console.log('here')
+exports.handler = async (event, context) => {
+ const url = 'https://vachana-backend.onrender.com/';
+
+ return new Promise((resolve, reject) => {
+   const req = https.get(url, (res) => {
+     if (res.statusCode === 200) {
+       resolve({
+         statusCode: 200,
+         body: 'Server pinged successfully',
+       });
+       console.log('done')
+     } else {
+       reject(
+         new Error(`Server ping failed with status code: ${res.statusCode}`)
+       );
+     }
+   });
+
+   req.on('error', (error) => {
+     reject(error);
+   });
+
+   req.end();
+ });
+};
   });
 
 function send() {
@@ -50,6 +75,10 @@ app.get("/api", (req, res)=> {
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.json(result);
+});
+
+app.post("/", (req, res) => {
+  res.send('successfully pinged!!');
 });
 
 app.listen(PORT, () => {
