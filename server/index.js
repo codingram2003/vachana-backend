3c4404f4972a174b21a4044d320edf32771ce7fa
Shriram <shriramrayakar@gmail.com>
2023-10-04 21:05:32 +0530

    random_push

--- server/index.js
+++ server/index.js
@@ -2,64 +2,43 @@ const express = require("express");
 var cron = require('node-cron');
 var mysql = require('mysql');
 const https = require('https');
+const createClient = require('@supabase/supabase-js')
 var result = []
 
-const dataFetch = async () => {
-  const response =  fetch('https://vachana-backend.onrender.com/api');
-  response.then(res =>
-    res.json()).then(d => {
-      console.log('here')
-    })
-};
-  cron.schedule('0 */10 * * * *', function() {
-    console.log('here');
-    dataFetch();
+const supabase = createClient.createClient(
+  "https://qmitfmhyxrdhvfhggjzj.supabase.co",
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtaXRmbWh5eHJkaHZmaGdnanpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTA5Nzc0MDYsImV4cCI6MjAwNjU1MzQwNn0.hcXJdGYSFJJwTUWbMKbkp7QCZSkV1EDBFxEzGucWWjo',
+  {auth: { persistSession: false }}   
 
-  });
-
-function send() {
-  var connection = mysql.createConnection({
-  host     : 'db4free.net',
-  user     : 'root2003',
-  password : 'root2003',
-  port     : '3306'
-});
-  connection.connect(function(err) {
-  if (err) {
-    console.error('Database connection failed: ' + err.stack);
-    return;
-  }
-
-  console.log('Connected to database.');
-});
-
-connection.query('SELECT * FROM demo_database.transcripts', (error, results, fields) => {
-  if (error) {
-    return console.error(error.message);
-  } else {
-    result=results
-    
-  }
-  
-});
-
-  connection.end();
-}
+);
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 
-app.get("/api", (req, res)=> {
-  console.log('got till here');
-    send();
+app.get("/guests", async(req, res)=> {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', true);
-    res.json(result);
+    const { data, error } = await supabase
+                                   .from('Guests')
+                                   .select()
+    res.json(data);
 });
+
+app.get("/events", async(req, res)=> {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    const { data, error } = await supabase
+                                   .from('Events')
+                                   .select()
+    res.json(data);
+});
+
 
 app.post("/", (req, res) => {
   res.send('successfully pinged!!');
