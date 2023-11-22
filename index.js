69d3bacb6530db30965adedbbf9e166b7aea2b49
Shriram <shriramrayakar@gmail.com>
2023-11-22 10:38:59 +0530

    team commit

--- index.js
+++ index.js
@@ -28,6 +28,17 @@ app.get("/guests", async(req, res)=> {
     res.json(data);
 });
 
+app.get("/team", async(req, res)=> {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  const { data, error } = await supabase
+                                 .from('Team')
+                                 .select()
+  res.json(data);
+});
+
 app.get("/events", async(req, res)=> {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
